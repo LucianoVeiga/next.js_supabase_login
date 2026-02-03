98f1e2377332fbe9308ec06746bc4ab41c613f14
bofficito <lautyboffi@gmail.com>
2026-02-02 22:46:00 -0300

    feat: Login with OTP

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,37 +4,88 @@
 import LoginForm from "@/components/LoginForm";
 import { useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
+  const router = useRouter();
+
+  const [step, setStep] = useState<"email" | "code">("email");
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [code, setCode] = useState("");
+  const [error, setError] = useState<string | null>(null);
 
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const { data, error } = await supabase.auth.signInWithPassword({
+    const { error } = await supabase.auth.signInWithOtp({
       email,
-      password,
     });
 
+    setLoading(false)
+
     if (error) {
-      setMessage(error.message);
-    } else {
-      setMessage("¡Inicio de sesión exitoso!");
+      setError('No pudimos enviar el código. Por favor, inténtalo de nuevo.');
+      return
     }
+
+    setStep("code");
+  };
+  
+  const handleVerifyOtp = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true)
+    setError(null)
+
+    const { error } = await supabase.auth.verifyOtp({
+      email,
+      token: code,
+      type: 'email'
+    })
+
+    setLoading(false)
+
+    if (error) {
+      setError('Código inválido o expirado');
+      return
+    }
+
+    router.push('/');
+    console.log('Usuario autenticado');
+    
   };
 
   return (
     <div>
-      <h2>Iniciar sesión</h2>
-      <LoginForm
-        handleLogin={handleLogin}
-        email={email}
-        setEmail={setEmail}
-        password={password}
-        setPassword={setPassword}
-		/>
-      {message && <p>{message}</p>}
-    </div>
+      {step === "email" && (
+      <form onSubmit={handleLogin}>
+        <input
+        type="text" 
+        required  
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        />
+        <button type="submit" disabled={loading}>
+          {loading ? 'Enviando...' : 'Enviar código'}
+        </button>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </form>
+      )}
+      {step === "code" && (
+      <form onSubmit={handleVerifyOtp}>
+        <input 
+        type="text"
+        placeholder="Codigo de 6 digitos"
+        value={code}
+        onChange={e => setCode(e.target.value)}
+        required
+        />
+        <button type="submit" disabled={loading}>
+          {loading ? 'Verificando...' : 'Verificar código'}
+        </button>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </form>
+      )}
+      </div>
+
   );
 }
